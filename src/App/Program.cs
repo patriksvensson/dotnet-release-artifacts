5821af1a5689b8bdfc504d667dd3c62470216395
Patrik Svensson <patrik@patriksvensson.se>
2020-03-22 02:17:21 +0100

    Initial commit

---
+++ src/App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_artifacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
